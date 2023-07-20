57123cf6f6ccc3c22a95099639ac8744a5bbd53d
JanetRW <jane_row@hotmail.com>
2023-07-20 15:22:55 +0200

    actualizando ficheros index y run

--- index.js
+++ index.js
@@ -8,8 +8,8 @@
 // -Crear un fichero run.js
 // •En el fichero index.js (fichero de librería):
 // -Declarar las funciones de la calculadora por separado (suma, resta, producto, división)
-function suma(op1,op2){
-    let result=op1+op2
+function suma(op1,op2,op3){
+    let result=op1+op2+op3;
     console.log(result);
 }
 
@@ -31,7 +31,7 @@ function cuadrado(op1,op2) {
     console.log(result);
   }
   
-suma(8,2);
+suma(8,2,30);
 resta(6,2);
 producto(5,3);
 division(18,9);
@@ -40,7 +40,7 @@ cuadrado(7,2)
 // -Al final del documento index.js escribir la sentencia:module.exports =
 // •A continuación escribir el nombre de las funciones entre {} separados por comas de la librería que se desean exportar.
 //module.exports={suma,resta,producto,division,cuadrado};
-module.exports={resta,producto,division,cuadrado};
+module.exports={suma,resta,producto,division,cuadrado};
 // •En el fichero run.js (fichero del programa principal) escribir la sentencia:-let myLib = require("./index");
 // •Llamar a todas las funciones de la librería e imprimir sus resultados.
 // -NOTA: Para llamar a la función hay que hacer myLib.función. Ej: myLib.suma(2,3)
